818e47d469ad0588c9fe5bbd3918689cd813461d
waheedali21 <waheedspc1@gmail.com>
2022-04-11 15:15:18 +0400

    ====

---
+++ TestMavenDemo12/src/test/java/com/demo/testcases/LoginTestCase2.java
@@ -0,0 +1,15 @@
+package com.demo.testcases;
+
+import org.testng.annotations.Test;
+
+public class LoginTestCase2 {
+	
+	
+	@Test
+	public void Signup() {
+	
+	//this is new update in master branch
+	
+		System.out.println("Signup Test");
+	}
+}
